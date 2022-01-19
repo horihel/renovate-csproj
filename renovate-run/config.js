7dc54a1e76d242f660956b36d249016ad4471671
Heiko Helmle <heiko.helmle@horiba.com>
2022-01-19 16:00:29 +0100

    add hostrules for www.example.com

--- renovate-run/config.js
+++ renovate-run/config.js
@@ -1,10 +1,18 @@
 module.exports = {
   token: process.env.TOKEN,
   repositories: ["horihel/renovate-csproj"],
+  hostRules: [
+    {
+      hostType: "nuget",
+      matchHost: "www.example.com",
+      username: "apikey",
+      password: "mysecret",
+    },
+  ],
   nuget: {
     registryUrls: [
       "https://api.nuget.org/v3/index.json",
-      "https://www.example.com/myexamplerepo"
+      "https://www.example.com/myexamplerepo",
     ],
   },
-}
+};
